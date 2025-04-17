669c0e82349cc1e2e0192f8a8ccd69502befffb6
Ahmad-younas <ahmadyounas2k18@gmail.com>
2025-04-17 21:35:43 +0500

    Removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import HeroSlider from './components/HeroSlider';
 import Navbar from "./components/Navbar";
-import Journey from './components/Journey';
 import Footer from './components/Footer';
 export default function Home() {
   return (
