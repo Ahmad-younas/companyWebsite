f6f2e1b6dad7497adac3647bb0bdbb2881aaa919
Ahmad-younas <ahmadyounas2k18@gmail.com>
2025-04-17 21:28:16 +0500

    Navbar ad Footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import HeroSlider from './components/HeroSlider';
+import Navbar from "./components/Navbar";
 import Journey from './components/Journey';
-
+import Footer from './components/Footer';
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <HeroSlider />
       <Journey />
+      <Footer />
     </main>
   );
 }
